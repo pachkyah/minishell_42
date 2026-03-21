b7e6027e9ddbcfa021ac227ea4c44d81bbd3f31f
Kseniia Amelina <xeniaket@gmail.com>
2026-03-21 16:19:25 +0100

    Update parser.c

--- src/parser.c
+++ src/parser.c
@@ -1,12 +1,38 @@
 #include "minishell.h"
 
-static int	calculate_len(char *line)
+/* Определяет длину обычного слова, учитывая кавычки.
+** Кавычки «прячут» спецсимволы внутри себя. */
+static int	get_token_len(char *line)
 {
-	if (*line == '|' || *line == '<' || *line == '>')
-		return (get_special_len(line));
-	return (get_token_len(line));
+	int		i;
+	char	quote;
+
+	i = 0;
+	quote = 0;
+	while (line[i])
+	{
+		if ((line[i] == '\'' || line[i] == '\"') && !quote)
+			quote = line[i];
+		else if (line[i] == quote)
+			quote = 0;
+		else if (!quote && (line[i] == ' ' || line[i] == '|' || 
+				 line[i] == '<' || line[i] == '>'))
+			break ;
+		i++;
+	}
+	return (i);
 }
 
+/* Определяет длину спецсимволов для редиректов и пайпов. */
+static int	get_special_len(char *line)
+{
+	if ((line[0] == '>' && line[1] == '>') || 
+		(line[0] == '<' && line[1] == '<'))
+		return (2);
+	return (1);
+}
+
+/* Выделяет память и копирует значение токена[cite: 25]. */
 static char	*copy_token_val(char *line, int len)
 {
 	char	*val;
@@ -25,6 +51,17 @@ static char	*copy_token_val(char *line, int len)
 	return (val);
 }
 
+/* Определяет тип токена для упрощения работы Person A[cite: 94]. */
+static t_token_type	get_token_type(char *val)
+{
+	if (val[0] == '|')
+		return (TOKEN_PIPE);
+	if (val[0] == '<' || val[0] == '>')
+		return (TOKEN_REDIR);
+	return (TOKEN_WORD);
+}
+
+/* Главная функция лексера: разбивает строку на список токенов. */
 t_token	*tokenize(char *line)
 {
 	t_token	*head;
@@ -34,15 +71,18 @@ t_token	*tokenize(char *line)
 	head = NULL;
 	while (*line)
 	{
-		while (*line == ' ')
+		while (*line == ' ' || (*line >= 9 && *line <= 13))
 			line++;
 		if (!*line)
 			break ;
-		len = calculate_len(line);
+		if (*line == '|' || *line == '<' || *line == '>')
+			len = get_special_len(line);
+		else
+			len = get_token_len(line);
 		val = copy_token_val(line, len);
 		if (!val)
-			return (NULL); //очистка
-		ft_token_add_back(&head, ft_new_token(val, get_token_type(*line)));
+			return (NULL); // В идеале тут должна быть функция очистки всего списка
+		ft_token_add_back(&head, ft_new_token(val, get_token_type(val)));
 		line += len;
 	}
 	return (head);
